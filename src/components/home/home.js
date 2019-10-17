222e8af611a3b8119fcd50aab7c1b712fed99e77
BASIL KURIAKOSE ROY <u180547@CS4-40>
2019-10-17 12:37:44 +0100

    Running hello

---
+++ src/components/home/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./home.css";
+class Home extends Component {
+  render() {
+    return (
+      <body>
+        <h1>Farm Safe Guide</h1>
+      </body>
+    );
+  }
+}
+export default Home;
---
+++ src/components/quiz/quiz.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Quiz extends Component {
+  render() {
+    return <h1>Lets see What you Learned</h1>;
+  }
+}
+export default Quiz;
