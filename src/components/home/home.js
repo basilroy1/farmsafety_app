8012ef197ca312e5ca1d2eb9772a8be237e111f6
basil roy <basil.k.roy@hotmail.com>
2019-10-21 14:03:06 +0100

    added bootstrap packages

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import "./home.css";
+import Badge from "react-bootstrap/Badge";
+import { Button } from "react-bootstrap";
 class Home extends Component {
   render() {
     return (
-      <body>
-        <h1>Farm Safe Guide</h1>
-      </body>
+      <div>
+        <body>
+          <h1>Farm Safe Guide</h1>
+          <Button variant="primary">
+            Profile <Badge variant="light">9</Badge>
+            <span className="sr-only">unread messages</span>
+          </Button>
+        </body>
+      </div>
     );
   }
 }
--- src/components/quiz/quiz.js
+++ src/components/quiz/quiz.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
-
+import pictures from "../pictures/health_hazard.jpg";
 class Quiz extends Component {
   render() {
-    return <h1>Lets se What you Learned</h1>;
+    return (
+      <div>
+        <h1>Lets see What you Learned</h1>
+        <img src={pictures} />
+        <span>sdsds</span>
+      </div>
+    );
   }
 }
 export default Quiz;
