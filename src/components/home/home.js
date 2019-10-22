bade39e3b02724a3a2d3698350f00365ffe86e9e
BASIL KURIAKOSE ROY <u180547@CS4-40>
2019-10-22 13:27:55 +0100

    added click function

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -8,12 +8,27 @@ import {
   Spinner
 } from "react-bootstrap";
 class Home extends Component {
+
+click =(e)=>{
+ console.log( <h3>logged in succesffuly</h3>);
+
+}
+
+
+
+
+
+
+
+
+
+
   render() {
     return (
       <div>
         <body>
           <div>
-            <Button variant="primary" size="lg" block>
+            <Button variant="primary" size="lg" block onClick={this.click}>
               Farm safety guide
             </Button>
           </div>
