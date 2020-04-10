42855cee6e29b2e731a31ac7e60518d43c4237f9
basil roy <basil.k.roy@hotmail.com>
2020-04-10 12:13:14 +0100

    cleaned up code

--- src/components/home/articles.js
+++ src/components/home/articles.js
@@ -55,12 +55,9 @@ class Articles extends Component {
             <Divider />
             <ExpansionPanelDetails>
               <Typography>
-                <p className="groove">
-                  All safety guards/ devices fitted The PTO "O" guards present
-                  Hydraulic systems and hoses in good repair All machinery
-                  defects identified and corrected Regular maintenance carried
-                  out
-                </p>
+                All safety guards/ devices fitted The PTO "O" guards present
+                Hydraulic systems and hoses in good repair All machinery defects
+                identified and corrected Regular maintenance carried out
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
@@ -80,12 +77,9 @@ class Articles extends Component {
             <Divider />
             <ExpansionPanelDetails>
               <Typography>
-                <p className="groove">
-                  All safety guards/ devices fitted The PTO "O" guards present
-                  Hydraulic systems and hoses in good repair All machinery
-                  defects identified and corrected Regular maintenance carried
-                  out
-                </p>
+                All safety guards/ devices fitted The PTO "O" guards present
+                Hydraulic systems and hoses in good repair All machinery defects
+                identified and corrected Regular maintenance carried out
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
@@ -105,15 +99,13 @@ class Articles extends Component {
             <Divider />
             <ExpansionPanelDetails>
               <Typography>
-                <p className="groove">
-                  Tractors and Machinery are the main cause of farm accidents in
-                  Ireland. Elderly farmers and children are at particular risk.
-                  Being entangled in PTO’s, crushed under a machine part, caught
-                  in a machine mechanism, crushed between vehicles and struck by
-                  a machine object are the main causes of deaths with farm
-                  machinery.
-                  <img src={piechart} alt="" />
-                </p>
+                Tractors and Machinery are the main cause of farm accidents in
+                Ireland. Elderly farmers and children are at particular risk.
+                Being entangled in PTO’s, crushed under a machine part, caught
+                in a machine mechanism, crushed between vehicles and struck by a
+                machine object are the main causes of deaths with farm
+                machinery.
+                <img className="chart" src={piechart} alt="" />
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
@@ -145,138 +137,6 @@ class Articles extends Component {
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
-          {/*   <Accordion defaultActiveKey="0">
-            <Card border="warning" id="header1">
-              <Card.Header>
-                <Accordion.Toggle
-                  as={Button}
-                  size="sm"
-                  block
-                  variant="link"
-                  eventKey="0"
-                >
-                  Guarding <GiSwordsEmblem /> <FaChevronRight />
-                </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="0">
-                <Card.Body>
-                  <p className="groove">
-                    Fixed guards must always be kept in place. These prevent
-                    entanglement The guard should ensure that no part of your
-                    body can reach the danger zone Fixed guarding of older
-                    machines should be upgraded in line with the guarding on
-                    newer models Do not use a machine unless all guards are in
-                    place
-                  </p>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card border="warning">
-              <Card.Header>
-                <Accordion.Toggle
-                  as={Button}
-                  size="sm"
-                  block
-                  variant="link"
-                  eventKey="1"
-                >
-                  Machinery Checks <MdCheckBox />
-                  <FaChevronRight />
-                </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="1">
-                <Card.Body>
-                  {" "}
-                  <p className="groove">
-                    All safety guards/ devices fitted The PTO "O" guards present
-                    Hydraulic systems and hoses in good repair All machinery
-                    defects identified and corrected Regular maintenance carried
-                    out
-                  </p>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card border="warning">
-              <Card.Header>
-                <Accordion.Toggle
-                  as={Button}
-                  size="sm"
-                  block
-                  variant="link"
-                  eventKey="2"
-                >
-                  Machinery Checks <MdCheckBox />
-                  <FaChevronRight />
-                </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="2">
-                <Card.Body>
-                  <p className="groove">
-                    All safety guards/ devices fitted The PTO "O" guards present
-                    Hydraulic systems and hoses in good repair All machinery
-                    defects identified and corrected Regular maintenance carried
-                    out
-                  </p>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card border="warning">
-              <Card.Header>
-                <Accordion.Toggle
-                  as={Button}
-                  size="sm"
-                  block
-                  variant="link"
-                  eventKey="3"
-                >
-                  Machinery Fatalities <MdCheckBox />
-                  <FaChevronRight />
-                </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="3">
-                <Card.Body>
-                  {" "}
-                  <p className="groove">
-                    Tractors and Machinery are the main cause of farm accidents
-                    in Ireland. Elderly farmers and children are at particular
-                    risk. Being entangled in PTO’s, crushed under a machine
-                    part, caught in a machine mechanism, crushed between
-                    vehicles and struck by a machine object are the main causes
-                    of deaths with farm machinery.
-                    <img src={piechart} alt="" />
-                  </p>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-
-            <Card border="warning">
-              <Card.Header>
-                <Accordion.Toggle
-                  as={Button}
-                  size="sm"
-                  block
-                  variant="link"
-                  eventKey="4"
-                >
-                  Farm Safety in News <MdCheckBox />
-                  <FaChevronRight />
-                </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="4">
-                <Card.Body>
-                  <iframe
-                    width="300"
-                    title="lol"
-                    height="315"
-                    src="https://www.youtube.com/embed/H9aQGovNlcU"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                  ></iframe>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-    </Accordion>*/}
         </div>
       </div>
     );
--- src/components/home/articles2.js
+++ src/components/home/articles2.js
@@ -31,13 +31,11 @@ class Articles extends Component {
             <Divider />
             <ExpansionPanelDetails>
               <Typography>
-                <p className="groove">
-                  Fixed guards must always be kept in place. These prevent
-                  entanglement The guard should ensure that no part of your body
-                  can reach the danger zone Fixed guarding of older machines
-                  should be upgraded in line with the guarding on newer models
-                  Do not use a machine unless all guards are in place
-                </p>
+                Fixed guards must always be kept in place. These prevent
+                entanglement The guard should ensure that no part of your body
+                can reach the danger zone Fixed guarding of older machines
+                should be upgraded in line with the guarding on newer models Do
+                not use a machine unless all guards are in place
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
@@ -57,12 +55,9 @@ class Articles extends Component {
             <Divider />
             <ExpansionPanelDetails>
               <Typography>
-                <p className="groove">
-                  All safety guards/ devices fitted The PTO "O" guards present
-                  Hydraulic systems and hoses in good repair All machinery
-                  defects identified and corrected Regular maintenance carried
-                  out
-                </p>
+                All safety guards/ devices fitted The PTO "O" guards present
+                Hydraulic systems and hoses in good repair All machinery defects
+                identified and corrected Regular maintenance carried out
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
@@ -82,12 +77,9 @@ class Articles extends Component {
             <Divider />
             <ExpansionPanelDetails>
               <Typography>
-                <p className="groove">
-                  All safety guards/ devices fitted The PTO "O" guards present
-                  Hydraulic systems and hoses in good repair All machinery
-                  defects identified and corrected Regular maintenance carried
-                  out
-                </p>
+                All safety guards/ devices fitted The PTO "O" guards present
+                Hydraulic systems and hoses in good repair All machinery defects
+                identified and corrected Regular maintenance carried out
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
@@ -107,15 +99,13 @@ class Articles extends Component {
             <Divider />
             <ExpansionPanelDetails>
               <Typography>
-                <p className="groove">
-                  Tractors and Machinery are the main cause of farm accidents in
-                  Ireland. Elderly farmers and children are at particular risk.
-                  Being entangled in PTO’s, crushed under a machine part, caught
-                  in a machine mechanism, crushed between vehicles and struck by
-                  a machine object are the main causes of deaths with farm
-                  machinery.
-                  <img src={piechart} alt="" />
-                </p>
+                Tractors and Machinery are the main cause of farm accidents in
+                Ireland. Elderly farmers and children are at particular risk.
+                Being entangled in PTO’s, crushed under a machine part, caught
+                in a machine mechanism, crushed between vehicles and struck by a
+                machine object are the main causes of deaths with farm
+                machinery.
+                <img className="chart" src={piechart} alt="" />
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
@@ -147,100 +137,6 @@ class Articles extends Component {
               </Typography>
             </ExpansionPanelDetails>
           </ExpansionPanel>
-          {/*<Accordion defaultActiveKey="0">
-            <Card>
-              <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                  Guarding 2 <GiSwordsEmblem /> <FaChevronRight />
-                </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="0">
-                <Card.Body>
-                  <p className="groove">
-                    Fixed guards must always be kept in place. These prevent
-                    entanglement The guard should ensure that no part of your
-                    body can reach the danger zone Fixed guarding of older
-                    machines should be upgraded in line with the guarding on
-                    newer models Do not use a machine unless all guards are in
-                    place
-                  </p>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                  Machinery Checks <MdCheckBox />
-                  <FaChevronRight />
-                </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="1">
-                <Card.Body>
-                  {" "}
-                  <p className="groove">
-                    All safety guards/ devices fitted The PTO "O" guards present
-                    Hydraulic systems and hoses in good repair All machinery
-                    defects identified and corrected Regular maintenance carried
-                    out
-                  </p>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="2">
-                  Machinery Checks <MdCheckBox />
-                  <FaChevronRight />
-                </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="2">
-                <Card.Body>
-                  <p className="groove">
-                    All safety guards/ devices fitted The PTO "O" guards present
-                    Hydraulic systems and hoses in good repair All machinery
-                    defects identified and corrected Regular maintenance carried
-                    out
-                  </p>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey="3">
-                  Machinery Fatalities <MdCheckBox />
-                  <FaChevronRight />
-                </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey="3">
-                <Card.Body>
-                  {" "}
-                  <p className="groove">
-                    Tractors and Machinery are the main cause of farm accidents
-                    in Ireland. Elderly farmers and children are at particular
-                    risk. Being entangled in PTO’s, crushed under a machine
-                    part, caught in a machine mechanism, crushed between
-                    vehicles and struck by a machine object are the main causes
-                    of deaths with farm machinery.
-                    <img src={piechart} alt="" />
-                  </p>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-          </Accordion>
-
-          <h3 style={{ marginLeft: "center " }} className="heading">
-            Farm Safety in News
-          </h3>
-
-          <iframe
-            width="300"
-            height="315"
-            title="lol"
-            src="https://www.youtube.com/embed/H9aQGovNlcU"
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-    ></iframe>*/}
         </div>
       </div>
     );
